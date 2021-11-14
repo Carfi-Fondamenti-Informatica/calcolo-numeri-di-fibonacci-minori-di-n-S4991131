091a265bba5fc05c3ebf1d6ed697403e702b4570
S4991131 <91836509+S4991131@users.noreply.github.com>
2021-11-14 16:23:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+   int n=0, a=0, b=1, c=0;
+   cin >> n;
+   
+   cout << b << endl;
+   for (c=0; c<n; c++){
+      c=a+b;
+      a=b;
+      b=c;
+   }   
    return 0;
 }
